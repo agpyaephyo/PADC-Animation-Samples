2bd25e826b6d86af18e6b092e8f048799f903df5
Aung Pyae Phyo <aungpyaephyo24@gmail.com>
2017-08-25 00:05:32 +0630

    drawable-animation

---
+++ app/src/main/java/xyz/aungpyaephyo/padc/animation/activities/DrawableAnimationActivity.java
@@ -0,0 +1,50 @@
+package xyz.aungpyaephyo.padc.animation.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import xyz.aungpyaephyo.padc.animation.R;
+
+/**
+ * Created by aung on 8/24/17.
+ */
+
+public class DrawableAnimationActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.iv_gyro)
+    ImageView ivGyro;
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, DrawableAnimationActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_animation);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.btn_animate_drawable)
+    public void onTapAnimateDrawable(View view) {
+        ivGyro.setBackgroundResource(R.drawable.gyro_anim);
+        AnimationDrawable animDrawableGyroImage = (AnimationDrawable) ivGyro.getBackground();
+        animDrawableGyroImage.start();
+    }
+}
