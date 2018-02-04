777c88a59462a23d56d358649004b5c3add5a39e
Aung Pyae Phyo <aungpyaephyo@mm-digital-solutions.com>
2018-02-04 17:23:48 +0630

    Api url update.

--- app/src/main/java/xyz/aungpyaephyo/padc/animation/utils/AppConstants.java
+++ app/src/main/java/xyz/aungpyaephyo/padc/animation/utils/AppConstants.java
@@ -6,12 +6,12 @@ package xyz.aungpyaephyo.padc.animation.utils;
 
 public class AppConstants {
 
-    public static final String ATTRACTION_BASE_URL = "http://www.aungpyaephyo.xyz/myanmar_attractions/";
+    public static final String ATTRACTION_BASE_URL = "http://padcmyanmar.com/padc-3/myanmar-attractions/apis/v1/";
     public static final String API_GET_ATTRACTION_LIST = "getAttractionsList.php";
 
     public static final String PARAM_ACCESS_TOKEN = "access_token";
 
     public static final String ACCESS_TOKEN = "b002c7e1a528b7cb460933fc2875e916";
 
-    public static final String BASE_IMAGE_PATH = "http://www.aungpyaephyo.xyz/myanmar_attractions/";
+    public static final String BASE_IMAGE_PATH = "http://padcmyanmar.com/padc-3/myanmar-attractions/images/";
 }
